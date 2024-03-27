3b8fb2f7b0b3a7112e240895779a87081799f94a
theendercore <theendercore@gmail.com>
2024-03-27 10:03:09 +0200

    - from properties to libs.ver - gradle 8.7

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,14 +1,8 @@
-val ktor_version: String by project
-val kotlin_version: String by project
-val logback_version: String by project
-
-val postgres_version: String by project
-val h2_version: String by project
 plugins {
     application
-    kotlin("jvm") version "1.9.22"
-    id("io.ktor.plugin") version "2.3.3"
-    id("org.jetbrains.kotlin.plugin.serialization") version "1.9.22"
+    alias(libs.plugins.kotlin.jvm)
+    alias(libs.plugins.ktor) version "2.3.3"
+    alias(libs.plugins.kotlin.plugin.serialization)
 }
 
 group = "org.teamvoided"
@@ -25,39 +19,35 @@ repositories {
     mavenCentral()
 }
 
-val arrow = "1.2.1"
 val sqldelight = "2.0.0"
-val semver_version = "1.4.2"
 
 dependencies {
-    implementation("io.arrow-kt", "arrow-core", arrow)
-    implementation("io.arrow-kt", "arrow-optics", arrow)
-    implementation("io.arrow-kt", "arrow-fx-coroutines", arrow)
+    implementation(libs.arrow.core)
+    implementation(libs.arrow.optics)
+    implementation(libs.arrow.fx.coroutines)
 
 
-    implementation("io.ktor", "ktor-server-core-jvm", ktor_version)
-    implementation("io.ktor", "ktor-server-netty", ktor_version)
-    implementation("io.ktor", "ktor-server-content-negotiation-jvm", ktor_version)
-    implementation("io.ktor", "ktor-serialization-kotlinx-json-jvm", ktor_version)
+    implementation(libs.ktor.server.core.jvm)
+    implementation(libs.ktor.server.netty)
+    implementation(libs.ktor.server.content.negotiation.jvm)
+    implementation(libs.ktor.serialization.kotlinx.json.jvm)
 
-    implementation("io.ktor", "ktor-server-html-builder", ktor_version)
-    implementation("org.jetbrains.kotlin-wrappers", "kotlin-css", "1.0.0-pre.705")
-
-
-
-    implementation("org.postgresql", "postgresql", postgres_version)
-    implementation("app.cash.sqldelight", "jdbc-driver", sqldelight)
-    implementation("app.cash.sqldelight", "postgresql-dialect", sqldelight)
+    implementation(libs.ktor.server.html.builder)
+    implementation(libs.kotlin.css)
 
 
-    implementation("com.h2database", "h2", h2_version)
-    implementation("ch.qos.logback", "logback-classic", logback_version)
+    implementation(libs.postgresql)
+    implementation(libs.jdbc.driver)
+    implementation(libs.postgresql.dialect)
 
 
+    implementation(libs.h2)
+    implementation(libs.logback)
 
-    testImplementation("io.ktor", "ktor-server-tests-jvm", ktor_version)
-    testImplementation("org.jetbrains.kotlin", "kotlin-test-junit", kotlin_version)
 
-    implementation("io.github.z4kn4fein","semver",semver_version)
+    testImplementation(libs.ktor.server.tests.jvm)
+    testImplementation(libs.kotlin.test.junit)
+
+    implementation(libs.semver)
 
 }
